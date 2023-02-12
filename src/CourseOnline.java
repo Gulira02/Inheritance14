d66f9ed6f46c57992ca8441da5110e474b84e283
user <gulira.abdukerimkyzy.jk@gmail.com>
2023-02-13 01:14:09 +0600

    Initial commit

---
+++ src/CourseOnline.java
@@ -0,0 +1,8 @@
+public class Course {
+    //Super класска 1 поле 1 метод түзүңүз.
+    private Information information;
+
+    public void coursePractice(){
+        System.out.println("");
+    }
+}
